4cc171db8ae11a37b39d488b893750922896125e
tomoaki0609 <asiasi.doba.dojo@gmail.com>
2017-10-12 13:36:34 +0900

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.9.1"
 
 set :application, "tabelog_doba"
-set :repo_url, "git@example.com:tomoaki0609/tabelog_doba.git"
+set :repo_url, "git@github.com:tomoaki0609/tabelog_doba.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.3.1'
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/Doba/.ssh/asikin.pem']
 
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_pid, -> { "/var/www/wantedly/shared/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
